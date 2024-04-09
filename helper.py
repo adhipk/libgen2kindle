a92810b8c3b52794e58a0aa9c42888aa3d27577d
adhipk <adhip.kashyap@gmail.com>
2024-04-08 20:59:18 -0700

    changed download dir to tmp

--- helper.py
+++ helper.py
@@ -1,13 +1,17 @@
+from io import BufferedRandom
+import tempfile
 import os
 from dotenv import load_dotenv
 from email.message import Message
 from urllib.request import urlretrieve
+from flask import request
 from libgen_api import LibgenSearch
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import requests
 
 load_dotenv()
 
@@ -19,7 +23,7 @@ smtp_server = os.getenv('SMTP_SERVER',"GSMTP")
 smtp_port = int(os.getenv('SMTP_PORT',587))
 subject = os.getenv('EMAIL_SUBJECT',"")
 body = os.getenv('EMAIL_BODY',"")
-download_path = os.getenv("DOWNLOAD_PATH","")
+download_path = "/tmp"
 
 s = LibgenSearch()
 extension = "epub"
@@ -35,7 +39,7 @@ def searchTitle(titleName:str) -> list[dict[str,str]]:
 def generateDownloadLinks(title):
 	return s.resolve_download_links(title)
 
-def sendEmail(filepath):
+def sendEmail(attachment,title):
 
 	# Create the email message
 	message = MIMEMultipart()
@@ -47,16 +51,14 @@ def sendEmail(filepath):
 	message.attach(MIMEText(body, 'plain'))
 
 	# Attach the file
-
-	with open(f"{filepath}", 'rb') as attachment:
-		part = MIMEBase('application', 'octet-stream')
-		part.set_payload(attachment.read())
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload(attachment)
 
 	# Encode the attachment in base64
 	encoders.encode_base64(part)
 
 	# Add header to the attachment
-	part.add_header('Content-Disposition', f"attachment; filename={filepath.split('/')[1]}")
+	part.add_header('Content-Disposition', f"attachment; filename={title}")
 
 	# Attach the file to the message
 	message.attach(part)
@@ -69,23 +71,34 @@ def sendEmail(filepath):
 		smtp_server.login(sender_email, password)
 		smtp_server.sendmail(sender_email, receiver_email, message.as_string())
 
+	# remove tmp file
+
 	print('Email sent!')
 def send2kindle(mirror:str,title:str):
 	try:
 		downloadLinks = generateDownloadLinks({"Mirror_1":mirror});
-		fileName = f"{title}.{extension}"
-		filepath = f"{download_path}/{fileName}"
-		urlretrieve(downloadLinks['GET'],filepath)
-		sendEmail(filepath)
+
+
+		# download file to tmp
+		with tempfile.NamedTemporaryFile(prefix=title,suffix=f".{extension}") as tmp:
+			if downloadLinks:
+				r = requests.get(downloadLinks["GET"],{"downloadFormat":extension})
+				tmp.write(r.content)
+				# seek(0) to read
+				tmp.seek(0)
+				attachment = tmp.read()
+				sendEmail(attachment,f"{tmp.name.split('/')[-1]}")
 	except Exception as e:
 		print(e)
 		return False
 	return True
 # def main():
-# 	titleName = 'Great Gatsby'
+# 	titleName = 'Pride and Prejudice'
 # 	res = searchTitle(titleName)
+# 	if not len(res):
+# 		return 0
 # 	book = res[0]
-# 	# print(book)
+
 # 	send2kindle(book['Mirror_1'],book['Title'])
 
 
